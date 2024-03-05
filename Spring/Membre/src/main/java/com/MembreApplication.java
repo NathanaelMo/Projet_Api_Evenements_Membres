4c6dcfd187170d8a9ca1919443e5a153cf209b52
NathanaelMo <monnier.nathanael29@gmail.com>
2024-03-05 16:14:57 +0100

    Spring api fonctionnelle avec war

--- Spring/Membre/src/main/java/com/MembreApplication.java
+++ Spring/Membre/src/main/java/com/MembreApplication.java
@@ -2,12 +2,19 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MembreApplication {
+public class MembreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MembreApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MembreApplication.class);
 	}
 
 }
